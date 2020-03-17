c6281ded4f7bee2617e6cc85fceb88cfb4c08ab3
davidlucky@gmail.com <davidlucky@gmail.com>
2020-03-17 07:45:39 -0500

    props entre componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import HolaMundo from "./components/HolaMundo";
-import AdiosMundo from "./components/AdiosMundo";
+// import HolaMundo from "./components/HolaMundo";
+// import AdiosMundo from "./components/AdiosMundo";
 import Saludar from "./components/Saludar";
 
 function App() {
@@ -12,13 +12,15 @@ function App() {
         color: "azul"
     };
 
-    const saludarFn = () => {};
+    const saludarFn = name => {
+        console.log("hola " + name);
+    };
 
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <Saludar userInfo={user} saludarFn={} />
+                <Saludar userInfo={user} saludarFn={saludarFn} />
                 {/* <Saludar name="Paco" edad="31" /> */}
             </header>
         </div>
--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,16 +1,17 @@
 import React from "react";
 
 export default function Saludar(props) {
-    const saludar = () => {
-        console.log("Hola");
-    };
+    console.log(props);
+
     return (
         <div>
             {/* <p>
                 Hola {props.userInfo.nombre}, tiene {props.userInfo.edad} años,
                 su color favorito es {props.userInfo.color}
             </p> */}
-            <button onClick={saludar}>Saludar</button>
+            <button onClick={() => props.saludarFn(props.userInfo.nombre)}>
+                Saludar
+            </button>
         </div>
     );
 }
